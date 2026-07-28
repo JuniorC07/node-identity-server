72dc4c8755aeb43fd01b99b999534881b8699823
Junior Mateus Alves Camargo <juniorc.dev@gmail.com>
2026-07-28 09:47:29 -0300

    feat: 🎸 add createUser controller

---
+++ src/controllers/createUser.ts
@@ -0,0 +1,24 @@
+import { CreateUser } from '@/useCases/CreateUser.js';
+import { CreateUserValidator } from '@/validators/users/createUser/index.js';
+import { type Request, type Response } from 'express';
+
+export class CreateUserController {
+  constructor(
+    private readonly createUser: CreateUser,
+    private readonly validator: CreateUserValidator
+  ) {}
+  handle = async (req: Request, res: Response): Promise<void> => {
+    const input = this.validator.validate(req.body);
+
+    const user = await this.createUser.execute(input);
+
+    res.status(201).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      login: user.login,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    });
+  };
+}
